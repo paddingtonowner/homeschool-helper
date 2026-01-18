691cfc0223bd56d23bb16e329d67e9727b502881
paddingtonowner <dianapatrice@gmail.com>
2026-01-18 16:11:19 -0500

    Format Mussolini's quote in HTML emphasis

--- quizzes/hitler_code.js
+++ quizzes/hitler_code.js
@@ -61,7 +61,7 @@ document.getElementById("answer_button_4").addEventListener("click",function() {
   } else if (input_4 === 'C') {
     answer_4 = "Wrong answer. Try again.";
   } else if (input_4 == 'D') {
-    answer_4 = "Correct! Hitler put the non-Germans into concentration camps, and those who couldn't were killed. The Italians had a similar feeling. Mussolini's famous quote: 'Everything inside the state; nothing outside the state; nothing against the state.'";
+    answer_4 = "Correct! Hitler put the non-Germans into concentration camps, and those who couldn't were killed. The Italians had a similar feeling. Mussolini's famous quote: <em>'Everything inside the state; nothing outside the state; nothing against the state.'</em>";
   } else if (input_4 === 'B') {
     answer_4 = "Wrong answer. Try again.";
   } else {
