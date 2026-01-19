596b7cfc0d3988bb6d4746e793b7633b4fdabdbc
paddingtonowner <dianapatrice@gmail.com>
2026-01-19 14:16:13 -0500

    Update answer feedback with HTML formatting

--- quizzes/hitler_code.js
+++ quizzes/hitler_code.js
@@ -3,13 +3,13 @@ var answer_1;
 document.getElementById("answer_button_1").addEventListener("click",function() {
   input_1 = document.getElementById("answer_1").value;
   if (input_1 === 'A') {
-    answer_1 = "The Germans were the ones who sank the <em>Lucitania</em>, but they might not have considered it an innocent ship. Try again.";
+    answer_1 = "<p class='wrong-answer'>The Germans were the ones who sank the <em>Lucitania</em>, but they might not have considered it an innocent ship. Try again.</p>";
   } else if (input_1 === 'C') {
-    answer_1 = "The Americans did wish the Germans Merry Christmas, but this probably did not breed resent.";
+    answer_1 = "<p class='wrong-answer'>The Americans did wish the Germans Merry Christmas, but this probably did not breed resent.</p>";
   } else if (input_1 == 'D') {
-    answer_1 = "The Americans did trespass on the Germans' territory, but this probably wasn't the one that bred the most resentment. Try again.";
+    answer_1 = "<p class='wrong-answer'>The Americans did trespass on the Germans' territory, but this probably wasn't the one that bred the most resentment. Try again.</p>";
   } else if (input_1 === 'B') {
-    answer_1 = "Correct! The Americans, after being forced into war, went on to win, and bred a lot of resentment in doing so.";
+    answer_1 = "<p class='correct-answer'>Correct! The Americans, after being forced into war, went on to win, and bred a lot of resentment in doing so.</p>";
   } else {
     answer_1 = "Please select A, B, C, or D for your answer (Not your grade).";
   }
@@ -21,13 +21,13 @@ var answer_2;
 document.getElementById("answer_button_2").addEventListener("click",function() {
   input_2 = document.getElementById("answer_2").value;
   if (input_2 === 'A') {
-    answer_2 = "Wrong answer. Try again.";
+    answer_2 = "<p class='wrong-answer'>Wrong answer. Try again.</p>";
   } else if (input_2 === 'C') {
-    answer_2 = "Wrong answer. Try again.";
+    answer_2 = "<p class='wrong-answer'>Wrong answer. Try again.</p>";
   } else if (input_2 == 'D') {
-    answer_2 = "Nice job remembering the alliances! The Italians had a similar belief to the Germans', but the Japanese just saw the alliance as a helpful weapon.";
+    answer_2 = "<p class='correct-answer'>Nice job remembering the alliances! The Italians had a similar belief to the Germans', but the Japanese just saw the alliance as a helpful weapon.</p>";
   } else if (input_2 === 'B') {
-    answer_2 = "Wrong answer. Try again.";
+    answer_2 = "<p class='wrong-answer'>Wrong answer. Try again.</p>";
   } else {
     answer_2 = "Please select A, B, C, or D for your answer (Not your grade).";
   }
@@ -39,13 +39,13 @@ var answer_3;
 document.getElementById("answer_button_3").addEventListener("click",function() {
   input_3 = document.getElementById("answer_3").value;
   if (input_3 === 'A') {
-    answer_3 = "Correct! Hitler thought that the <em>true</em> Germans had been too watered down with other races.";
+    answer_3 = "<p class='correct-answer'>Correct! Hitler thought that the <em>true</em> Germans had been too watered down with other races.</p>";
   } else if (input_3 === 'C') {
-    answer_3 = "Wrong answer. Try again.";
+    answer_3 = "<p class='wrong-answer'>Wrong answer. Try again.</p>";
   } else if (input_3 == 'D') {
-    answer_3 = "Wrong answer. Try again.";
+    answer_3 = "<p class='wrong-answer'>Wrong answer. Try again.</p>";
   } else if (input_3 === 'B') {
-    answer_3 = "Wrong answer. Try again.";
+    answer_3 = "<p class='wrong-answer'>Wrong answer. Try again.</p>";
   } else {
     answer_3 = "Please select A, B, C, or D for your answer (Not your grade).";
   }
@@ -57,13 +57,13 @@ var answer_4;
 document.getElementById("answer_button_4").addEventListener("click",function() {
   input_4 = document.getElementById("answer_4").value;
   if (input_4 === 'A') {
-    answer_4 = "Wrong answer. Try again.";
+    answer_4 = "<p class='wrong-answer'>Wrong answer. Try again.</p>";
   } else if (input_4 === 'C') {
-    answer_4 = "Wrong answer. Try again.";
+    answer_4 = "<p class='wrong-answer'>Wrong answer. Try again.</p>";
   } else if (input_4 == 'D') {
-    answer_4 = "Correct! Hitler put the non-Germans into concentration camps, and those who couldn't were killed.";
+    answer_4 = "<p class='correct-answer'>Correct! Hitler put the non-Germans into concentration camps, and those who couldn't were killed.</p>";
   } else if (input_4 === 'B') {
-    answer_4 = "Wrong answer. Try again.";
+    answer_4 = "<p class='wrong-answer'>Wrong answer. Try again.</p>";
   } else {
     answer_4 = "Please select A, B, C, or D for your answer (Not your grade).";
   }
@@ -75,13 +75,13 @@ var answer_5;
 document.getElementById("answer_button_5").addEventListener("click",function() {
   input_5 = document.getElementById("answer_5").value;
   if (input_5 === 'A') {
-    answer_5 = "Wrong answer. Try again.";
+    answer_5 = "<p class='wrong-answer'>Wrong answer. Try again.</p>";
   } else if (input_5 === 'D') {
-    answer_5 = "Wrong answer. Try again.";
+    answer_5 = "<p class='wrong-answer'>Wrong answer. Try again.</p>";
   } else if (input_5 == 'C') {
-    answer_5 = "Correct! Hitler thought: 'A few human lives don't matter when the nation being strong is at stake'.<br>The Italians had a similar feeling. Mussolini's famous quote: <em>'Everything inside the state; nothing outside the state; nothing against the state.'</em>";
+    answer_5 = "<p class='correct-answer'>Correct! Hitler thought: 'A few human lives don't matter when the nation being strong is at stake'.<br>The Italians had a similar feeling. Mussolini's famous quote: <em>'Everything inside the state; nothing outside the state; nothing against the state.'</em></p>";
   } else if (input_5 === 'B') {
-    answer_5 = "Wrong answer. Try again.";
+    answer_5 = "<p class='wrong-answer'>Wrong answer. Try again.</p>";
   } else {
     answer_5 = "Please select A, B, C, or D for your answer (Not your grade).";
   }
